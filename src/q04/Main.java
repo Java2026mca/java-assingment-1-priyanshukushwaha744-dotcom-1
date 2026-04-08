653f75f4cad49a6f596c75569b0d018b90e80b7c
priyanshukushwaha744-dotcom <priyanshukushwaha744@gmail.com>
2026-04-08 13:46:38 +0530

    Update Main.java

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -36,20 +36,6 @@ public class Main {
         }
     }
 }
-        // TODO: Read n integers. For each number print:
-        //   "Prime"     if it is prime
-        //   "Perfect"   if it is a perfect number (sum of proper divisors == itself, e.g. 6=1+2+3)
-        //   "Both"      if it is both (there are none < 100, but handle it)
-        //   "Neither"   otherwise
-        //
-        // Input:
-        // 4
-        // 6 13 8 28
-        //
-        // Output:
-        // Perfect
-        // Prime
-        // Neither
         // Perfect
 
     }
