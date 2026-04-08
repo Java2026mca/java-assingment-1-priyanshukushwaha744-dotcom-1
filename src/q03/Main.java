471890f6140b4a579f7a1a02e3c01c50ecb68409
priyanshukushwaha744-dotcom <priyanshukushwaha744@gmail.com>
2026-04-08 13:46:02 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -43,17 +43,3 @@ public class Main {
         System.out.println("Diagonal: " + diagonalSum);
     }
 }
-
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
-
-    }
-}
